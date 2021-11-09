60414eb30ab18794568b0e80fc95a49b86ca34f9
kegly <90460093+kegly@users.noreply.github.com>
2021-11-09 14:59:04 +0300

    Add files via upload

---
+++ 3(buble).py
@@ -0,0 +1,22 @@
+f=open('input3')
+n, k=map(int, f.readline().split())
+A=[int(i) for i in f.readline().split()]
+
+for i in range(len(A)):
+    for j in range(len(A) - i - 1):
+        if A[j] > A[j + k]:
+            A[j], A[j + k] = A[j + k], A[j]
+
+# check :
+file= open('output3', 'w')
+flag=True
+for i in range(len(A)-1):
+    if A[i]>A[i+1]:
+        flag=False
+        break
+if flag:
+    file.write('YES')
+else:
+    file.write('NO')
+
+file.close()
